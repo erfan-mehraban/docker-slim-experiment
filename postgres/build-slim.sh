cfa4a2886fd77009f14d9584e1dc0fbb6aa40fdf
erfan.mehraban <erfan.mehraban@gmail.com>
2022-05-31 20:57:07 +0430

    Add benchamark

--- postgres/build-slim.sh
+++ postgres/build-slim.sh
@@ -3,6 +3,8 @@ docker-slim build \
     --env POSTGRES_HOST_AUTH_METHOD=trust \
     --show-clogs=true \
     --http-probe=false \
-    --exec "pgbench -i -U postgres -s 10 postgres && pgbench -U postgres -c 10 -t 2000 postgres"
+    --mount /tmp/pgdata/:/var/lib/postgresql/data/ \
+    --exclude-mounts \
+    --exec "pgbench -i -U postgres -s 10 postgres && pgbench -U postgres -c 10 -t 2000 postgres && psql -U postgres postgres -c 'DROP TABLE pgbench_accounts, pgbench_branches, pgbench_tellers, pgbench_history;'"
 
 cat slim.report.json | jq .artifact_location | xargs -I{} cp -r {} .
--- redis/benchamrk.sh
+++
@@ -1,9 +0,0 @@
-dstat -lcmdrsyTt --full --out dtat-result-orginal.csv &
-dstas_pid=$!
-docker-compose -f docker-compose-original.yml up --abort-on-container-exit
-kill -9 $dstas_pid
-
-dstat -lcmdrsyTt --full --out dtat-result-slim.csv &
-dstas_pid=$!
-docker-compose -f docker-compose-slim.yml up --abort-on-container-exit
-kill -9 $dstas_pid
